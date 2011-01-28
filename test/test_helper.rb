eabb1db02c21a31dc5a0b8f7c7522c9ee78d7fe3
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-01-28 10:50:39 -0200

    cleaning up requires on test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,5 @@ require 'rubygems'
 require 'ruby-debug'
 require "methodize"
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/tokamak.rb')
+require 'lib/tokamak'
 
--- test/tokamak/builder/json_test.rb
+++ test/tokamak/builder/json_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+require 'test_helper'
 
 class Tokamak::Builder::JsonTest < Test::Unit::TestCase
 
