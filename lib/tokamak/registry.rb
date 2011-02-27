2581bbdc8796fd7c132408d3beb5677d3ae59a2d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-02-27 03:14:47 -0300

    adding registry

---
+++ lib/tokamak/registry.rb
@@ -0,0 +1,19 @@
+module Tokamak
+  class Registry
+
+    def initialize
+      @media_types = {}
+    end
+
+    def <<(handler)
+      handler.media_types.each do |type|
+        @media_types[type] = handler
+      end
+    end
+
+    def [](media_type)
+      @media_types[media_type[/^([^\s\;]+)/, 1]]
+    end
+
+  end
+end
